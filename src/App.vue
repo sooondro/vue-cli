09941d00456e384b004acac8e24d6da8b5967434
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-13 08:26:50 +0100

    vue.js - CLI

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-header></app-header>
+    
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue'
+
+
+export default {
+  components:{
+    'app-header': Header,
+    'app-footer': Footer
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" v-on:click="ninja.show=!ninja.show">
+        <h2>{{ ninja.name }}</h2>
+        <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}   ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
