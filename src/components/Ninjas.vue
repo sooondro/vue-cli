5225936fbde06a5205c9e219bd1da4cd27b4cdd9
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-13 08:33:08 +0100

    Nesting components

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -27,5 +27,25 @@ export default {
 </script>
 
 <style scoped>
-
+#ninjas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+}
 </style>
