1906af0362f3a4d60a8804a6def6924f0506e57d
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-15 23:12:01 +0100

    Custom search filter - blog

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <div v-theme:column="'wide'" id="show-blogs">
     <h1>All Blogs</h1>
-    <div v-for="blog in blogs" class="single-blog">
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for="blog in filteredBlogs" class="single-blog">
       <h2 v-rainbow>{{ blog.title | to-uppercase}}</h2>
       <article>{{ blog.body | snippet }}</article>
     </div>
@@ -15,7 +16,8 @@
 export default {
   data () {
     return {
-      blogs: []
+      blogs: [],
+      search: ""
     }
   },
   methods: {
@@ -25,6 +27,13 @@ export default {
     this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
       this.blogs = data.body.slice(0,10);
     })
+  },
+  computed: {
+    filteredBlogs: function(){
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      })
+    }
   }
 }
 </script>
